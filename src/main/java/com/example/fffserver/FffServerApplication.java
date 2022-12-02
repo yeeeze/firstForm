a1c6fa52a8485d475cd254cb3bc2436498eb04cd
yeezze <yeeeze22@gmail.com>
2022-12-02 16:30:23 +0900

    first commit

---
+++ src/main/java/com/example/fffserver/FffServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fffserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FffServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FffServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fffserver/FffServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fffserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FffServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
