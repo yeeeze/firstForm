8522f7ed85f3f731bfb3305afc395437e716aa50
yeezze <yeeeze22@gmail.com>
2022-12-04 23:43:24 +0900

    feat: DB 변경 (mysql -> mongoDB)

--- src/main/java/com/example/fffserver/FffServerApplication.java
+++ src/main/java/com/example/fffserver/FffServerApplication.java
@@ -2,8 +2,12 @@ package com.example.fffserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableMongoAuditing
 public class FffServerApplication {
 
 	public static void main(String[] args) {
