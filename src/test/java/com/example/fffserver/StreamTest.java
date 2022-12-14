6ab121a6fcab120cd0e462c0ab87e0c1d54f5847
yeezze <yeeeze22@gmail.com>
2022-12-14 22:04:10 +0900

    test: StreamTest 병렬처리 순서 테스트 추가하여 확인

--- src/test/java/com/example/fffserver/StreamTest.java
+++ src/test/java/com/example/fffserver/StreamTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 class StreamTest {
@@ -39,4 +40,42 @@ class StreamTest {
             });
         }
     }
+
+    @Nested
+    @DisplayName("SubmissionService publish()와 비슷한 환경에서 병렬테스트")
+    class Parallel {
+
+        @Test
+        @DisplayName("user 로직은 순차 처리")
+        void parallelStreamTest1() {
+            Map<String, Integer> map = Map.of("1", 1, "2", 2);
+            Map<Integer, Set<String>> setMap = Map.of(1, Set.of("11", "12"), 2, Set.of("22", "21"));
+
+            map.entrySet().parallelStream().forEach(entry -> {
+                Integer value = entry.getValue();
+
+                Set<String> strings = setMap.get(value);
+                for (String s : strings) {
+                    log.info("이벤트: {}, 유저: {}", value, s);
+                }
+            });
+        }
+
+        @Test
+        @DisplayName("user 로직도 병렬 처리")
+        void parallelStreamTest2() {
+            Map<String, Integer> map = Map.of("1", 1, "2", 2);
+            Map<Integer, Set<String>> setMap = Map.of(1, Set.of("11", "12"), 2, Set.of("22", "21"));
+
+            map.entrySet().parallelStream().forEach(entry -> {
+                Integer value = entry.getValue();
+
+                Set<String> strings = setMap.get(value);
+                strings.parallelStream().forEach((s) -> {
+                    log.info("이벤트: {}, 유저: {}", value, s);
+                });
+            });
+        }
+    }
+
 }
