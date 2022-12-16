5ea801fe65681fac6f3b600e19a12f2d4c5b1737
yeezze <yeeeze22@gmail.com>
2022-12-17 01:16:08 +0900

    feat: 환경에 따라 QueueScheduler의 실행여부 설정 추가
    - 일부 테스트 환경에서 스케줄러 작업을 제외시키기 위함

--- src/main/java/com/example/fffserver/domain/common/QueueScheduler.java
+++ src/main/java/com/example/fffserver/domain/common/QueueScheduler.java
@@ -2,10 +2,12 @@ package com.example.fffserver.domain.common;
 
 import com.example.fffserver.domain.answer.domain.SubmissionService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(value = "scheduler.enable", havingValue = "true", matchIfMissing = true)
 @Slf4j
 public class QueueScheduler {
 
