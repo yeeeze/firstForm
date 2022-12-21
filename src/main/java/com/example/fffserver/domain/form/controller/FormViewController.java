07d18919114b45ee8e057d885936c9b1df1ff69b
yeezze <yeeeze22@gmail.com>
2022-12-22 02:08:59 +0900

    feat: 입장 대기 페이지 구현
    - 시작 시간 이전에 입장 불가능

--- src/main/java/com/example/fffserver/domain/form/controller/FormViewController.java
+++ src/main/java/com/example/fffserver/domain/form/controller/FormViewController.java
@@ -19,11 +19,27 @@ public class FormViewController {
         this.formService = formService;
     }
 
+    /**
+     * 입장 대기 페이지
+     */
     @GetMapping("/api/v1/form/{formId}")
+    public String viewEnterPage(@PathVariable String formId, Model model) {
+        Form form = formService.findById(formId);
+        model.addAttribute("formId", form.getId().toString());
+        model.addAttribute("startTime", form.getStartTime());
+        model.addAttribute("winnersNum", form.getWinnersNumber());
+        return "entrance";
+    }
+
+    /**
+     * 응답 작성 페이지
+     */
+    @GetMapping("/api/v1/form/{formId}/enter")
     public String viewSubmitPage(@PathVariable String formId, Model model) {
         Form form = formService.findById(formId);
         List<Question> questions = form.getQuestions();
         model.addAttribute("form", form);
+        model.addAttribute("formId", form.getId().toString());
         model.addAttribute("questions", questions);
         return "submitForm";
     }
