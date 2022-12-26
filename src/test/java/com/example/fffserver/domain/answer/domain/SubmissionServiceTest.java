c85363e97993c56112b92574a5651934c39f651e
yeezze <yeeeze22@gmail.com>
2022-12-26 22:59:15 +0900

    test: SubmissionServiceTest 수정
    - 테스트 순서 지정, tearDown 클래스 단위로 AfterAll 처리

--- src/test/java/com/example/fffserver/domain/answer/domain/SubmissionServiceTest.java
+++ src/test/java/com/example/fffserver/domain/answer/domain/SubmissionServiceTest.java
@@ -5,6 +5,7 @@ import com.example.fffserver.domain.form.domain.FormRepository;
 import com.example.fffserver.domain.form.domain.entity.Form;
 import com.example.fffserver.domain.form.domain.vo.Event;
 import com.example.fffserver.domain.form.domain.vo.EventFactory;
+import com.example.fffserver.domain.question.domain.QuestionReposioty;
 import com.example.fffserver.domain.question.domain.entity.Question;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Awaitility;
@@ -15,7 +16,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -27,6 +27,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class SubmissionServiceTest {
 
     @Autowired
@@ -35,6 +37,10 @@ class SubmissionServiceTest {
     RedisTemplate<String, String> redisTemplate;
     @Autowired
     FormRepository formRepository;
+    @Autowired
+    QuestionReposioty questionReposioty;
+    @Autowired
+    AnswerRepository answerRepository;
 
 
     private List<String> keys = new ArrayList<>();
@@ -43,15 +49,18 @@ class SubmissionServiceTest {
     void setUp() {
     }
 
-    @AfterEach
+    @AfterAll
     void tearDown() {
-        formRepository.deleteAll();
         for (String key : keys) {
             redisTemplate.delete(key);
         }
+        formRepository.deleteAll();
+        answerRepository.deleteAll();
+        questionReposioty.deleteAll();
     }
 
     @Nested
+    @Order(1)
     @DisplayName("이벤트가 1개일 때")
     class oneEvent {
 
@@ -64,7 +73,7 @@ class SubmissionServiceTest {
             final int winner = 30;
             final CountDownLatch countDownLatch = new CountDownLatch(user);
 
-            form = new Form(LocalDateTime.now(), LocalDateTime.of(2022, Month.DECEMBER, 30, 23, 59), winner, List.of(new Question("유저 이름", "TEXT")));
+            form = new Form(LocalDateTime.now(), LocalDateTime.now().plusDays(2L), winner, List.of(new Question("유저 이름", "TEXT", null)));
             formRepository.insert(form);
             keys.add(form.getId().toString());
 
@@ -76,15 +85,17 @@ class SubmissionServiceTest {
             workers.forEach(Thread::start);
             countDownLatch.await();
 
-            Awaitility.await().atMost(Duration.ofMinutes(10))
+            Awaitility.await().atMost(Duration.ofMinutes(5))
                     .untilAsserted(() -> {
                         Long waitingQueueSize = submissionService.getWaitingQueueSize(event);
+                        log.info("남은 인원 {}",waitingQueueSize);
                         assertThat(waitingQueueSize).isEqualTo(user - winner);
                     });
         }
     }
 
     @Nested
+    @Order(2)
     @DisplayName("다수의 이벤트가 동시에 진행될 때")
     class manyEvent {
 
@@ -99,11 +110,11 @@ class SubmissionServiceTest {
             final CountDownLatch countDownLatch1 = new CountDownLatch(user);
             final CountDownLatch countDownLatch2 = new CountDownLatch(user);
 
-            formFirst = new Form(LocalDateTime.now(), LocalDateTime.of(2022, Month.DECEMBER, 30, 23, 59), winner, List.of(new Question("유저 이름", "TEXT")));
+            formFirst = new Form(LocalDateTime.now(), LocalDateTime.now().plusDays(2L), winner, List.of(new Question("유저 이름", "TEXT", null)));
             formRepository.insert(formFirst);
             keys.add(formFirst.getId().toString());
 
-            formSecond = new Form(LocalDateTime.now(), LocalDateTime.of(2022, Month.DECEMBER, 30, 23, 59), winner, List.of(new Question("유저 이름", "TEXT")));
+            formSecond = new Form(LocalDateTime.now(), LocalDateTime.now().plusDays(2L), winner, List.of(new Question("유저 이름", "TEXT", null)));
             formRepository.insert(formSecond);
             keys.add(formSecond.getId().toString());
 
@@ -121,10 +132,12 @@ class SubmissionServiceTest {
             countDownLatch1.await();
             countDownLatch2.await();
 
-            Awaitility.await().atMost(Duration.ofMinutes(10))
+            Awaitility.await().atMost(Duration.ofMinutes(5))
                     .untilAsserted(() -> {
                         Long waitingQueueSize1 = submissionService.getWaitingQueueSize(event1);
+                        log.info("남은 인원 {}",waitingQueueSize1);
                         Long waitingQueueSize2 = submissionService.getWaitingQueueSize(event2);
+                        log.info("남은 인원 {}",waitingQueueSize2);
 
                         assertThat(waitingQueueSize1).isEqualTo(user - winner);
                         assertThat(waitingQueueSize2).isEqualTo(user - winner);
