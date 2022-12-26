cd411621bb9528ce428b5f8d22c5d1d0a7ea3be7
yeezze <yeeeze22@gmail.com>
2022-12-26 20:07:09 +0900

    test: mongo test 수정

--- src/test/java/com/example/fffserver/MongoTest.java
+++ src/test/java/com/example/fffserver/MongoTest.java
@@ -1,13 +1,10 @@
 package com.example.fffserver;
 
-import com.example.fffserver.domain.form.application.FormService;
 import com.example.fffserver.domain.form.domain.FormRepository;
 import com.example.fffserver.domain.form.domain.entity.Form;
 import com.example.fffserver.domain.question.domain.entity.Question;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
@@ -19,34 +16,36 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
-class FormServiceTest {
+@DisplayName("MongoDB CRUD test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+class MongoTest {
 
     @Autowired
     FormRepository formRepository;
-    FormService formService;
 
-    @BeforeEach
-    void setUp() {
-        this.formService = new FormService(formRepository);
-    }
+    ObjectId formId;
 
-    @AfterEach
-    void tearDown() {
-        formService.deleteAll();
-    }
-
+    @Order(1)
     @Test
-    @DisplayName("insert 성공")
+    @DisplayName("insert 성공, 영속성 컨텍스트 관리되지 않음 확인")
     void insertForm() {
         List<Question> questions = new ArrayList<>();
-        questions.add(new Question("학번", "TEXT"));
-        questions.add(new Question("이름", "TEXT"));
+        questions.add(new Question("학번", "TEXT", null));
+        questions.add(new Question("이름", "TEXT", null));
         Form form = new Form(LocalDateTime.now(), LocalDateTime.of(2022, Month.DECEMBER, 7, 23, 59), 20, questions);
 
-        formService.insert(form);
+        Form save = formRepository.save(form);
+        formId = save.getId();
+        Form find = formRepository.findById(save.getId()).get();
 
-        List<Form> all = formService.findAll();
-        assertThat(all).hasSize(1);
-        assertThat(all.get(0).getId()).isEqualTo(form.getId());
+        assertThat(find.getId()).isEqualTo(save.getId());
+        assertThat(find).isNotEqualTo(save);
+    }
+
+    @Order(2)
+    @Test
+    void tearDown() {
+        formRepository.deleteById(formId);
     }
 }
