7b737b17763024991e63f6dbc10c5f79c95c5a39
yeezze <yeeeze22@gmail.com>
2023-01-01 23:30:55 +0900

    feat: 애플리케이션 초기화 시 TimeZone 설정 추가

--- src/main/java/com/example/fffserver/FffServerApplication.java
+++ src/main/java/com/example/fffserver/FffServerApplication.java
@@ -7,11 +7,19 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableMongoRepositories
 @EnableMongoAuditing
 @EnableScheduling
 public class FffServerApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FffServerApplication.class, args);
