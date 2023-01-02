5cb9184e19f8a744ff6acea043b0d3ba9899bcc5
yeezze <yeeeze22@gmail.com>
2023-01-03 00:06:03 +0900

    feat: cors 설정 추가

--- src/main/java/com/example/fffserver/FffServerApplication.java
+++ src/main/java/com/example/fffserver/FffServerApplication.java
@@ -3,9 +3,12 @@ package com.example.fffserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -25,4 +28,13 @@ public class FffServerApplication {
 		SpringApplication.run(FffServerApplication.class, args);
 	}
 
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://www.first-form.shop");
+			}
+		};
+	}
 }
