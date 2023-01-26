c731a05ceb3e5a099a62ca9ce6066d9bcaeeb398
yeezze <yeeeze22@gmail.com>
2023-01-26 18:45:33 +0900

    webhook test

--- src/main/java/com/example/fffserver/domain/answer/domain/AnswerRepository.java
+++ src/main/java/com/example/fffserver/domain/answer/domain/AnswerRepository.java
@@ -1,14 +1,13 @@
 package com.example.fffserver.domain.answer.domain;
 
 import com.example.fffserver.domain.answer.domain.entity.Answer;
+import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AnswerRepository extends MongoRepository<Answer, ObjectId> {
 
-     List<Answer> findAllByFormId(ObjectId formId);
+  List<Answer> findAllByFormId(ObjectId formId);
 }
