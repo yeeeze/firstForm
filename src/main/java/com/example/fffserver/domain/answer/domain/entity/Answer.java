7561a7a61e5e250189e809aab7784ac9fb3ea2b0
yeezze <yeeeze22@gmail.com>
2023-01-26 18:37:38 +0900

    test: webhook test

--- src/main/java/com/example/fffserver/domain/answer/domain/entity/Answer.java
+++ src/main/java/com/example/fffserver/domain/answer/domain/entity/Answer.java
@@ -1,37 +1,36 @@
 package com.example.fffserver.domain.answer.domain.entity;
 
 import com.example.fffserver.domain.common.BaseEntity;
+import javax.persistence.Id;
 import lombok.Getter;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import javax.persistence.Id;
 
 @Getter
 @Document("answer")
 public class Answer extends BaseEntity {
 
-    @Id
-    private ObjectId id;
-    private String userId;
-    private String content;
-    private ObjectId formId;
-    private ObjectId questionId;
+  @Id
+  private ObjectId id;
+  private String userId;
+  private String content;
+  private ObjectId formId;
+  private ObjectId questionId;
 
-    public Answer() {
-    }
+  public Answer() {
+  }
 
-    public Answer(String userId, String content, ObjectId formId) {
-        this.userId = userId;
-        this.content = content;
-        this.formId = formId;
-    }
+  public Answer(String userId, String content, ObjectId formId) {
+    this.userId = userId;
+    this.content = content;
+    this.formId = formId;
+  }
 
-    public void mappedQuestionId(ObjectId questionId) {
-        this.questionId = questionId;
-    }
+  public void mappedQuestionId(ObjectId questionId) {
+    this.questionId = questionId;
+  }
 
-    public ObjectId getFormId() {
-        return formId;
-    }
+  public ObjectId getFormId() {
+    return formId;
+  }
 }
