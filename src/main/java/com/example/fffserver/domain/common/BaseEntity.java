de37d8978fc8f0d5f89e92692822e9f6416bb07b
yeezze <yeeeze22@gmail.com>
2023-01-27 11:28:39 +0900

    webhook test

--- src/main/java/com/example/fffserver/domain/common/BaseEntity.java
+++ src/main/java/com/example/fffserver/domain/common/BaseEntity.java
@@ -1,13 +1,12 @@
 package com.example.fffserver.domain.common;
 
+import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
 
 @Getter
 public abstract class BaseEntity {
 
-    @CreatedDate
-    private LocalDateTime createdAt;
+  @CreatedDate
+  private LocalDateTime createdAt;
 }
